7dfd9f65d419f06b9d2e6df31d9e4287198a6bf2
teemal <taymal1987@gmail.com>
2019-11-22 19:03:28 -0800

    stripped comma

--- scam_boi.py
+++ scam_boi.py
@@ -9,17 +9,19 @@ url = "https://eslplaygames.com/openid/ZMR38yLzJbGUWBg65BLhL4BP"
 chars = string.ascii_letters + string.digits + '^%$!()'
 random.seed = (os.urandom(1024))
 
-names = json.loads(open('names.json')).read()
-for name in names:
+n = open('names.txt', "r")
+for name in n:
+    names = name.split()
     for i in names:
-        extra = ''.join(random.choice(string.digits))
+       for j in names:
+           for k in names:
+                name_boi = name.split(",")
+                uname = name_boi[0].lower() + str(random.randint(3,99))
+                pass_boi = ''.join(random.choice(chars) for i in range(5))
 
-        uname = name.lower() + extra
-        pass_boi = ''.join(random.choice(chars) for i in range(5))
+                requests.post(url, allow_redirects=False, data={
+                    'username': uname,
+                    'password': pass_boi
+                })
 
-        requests.post(url, allow_redirects=False, data={
-            "username": uname,
-            "password": pass_boi
-        })
-
-        print(("username: {0} \npassword: {1} \n").format(uname, pass_boi))
+                print(("username: {0} \npassword: {1} \n").format(uname, pass_boi))
